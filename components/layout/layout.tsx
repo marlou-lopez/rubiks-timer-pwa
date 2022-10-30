33b93e688f62e4e63bdca117490f1f2ba1d88e76
Marlou Lopez <dev.marlou.lopez@gmail.com>
2022-10-31 00:42:57 +0800

    add selected indicator for nav

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,4 +1,10 @@
-import { ClockIcon, AcademicCapIcon } from '@heroicons/react/20/solid';
+import {
+  ClockIcon,
+  AcademicCapIcon,
+  ListBulletIcon,
+  BookmarkIcon,
+  BookOpenIcon,
+} from '@heroicons/react/20/solid';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -7,6 +13,8 @@ const PrimaryLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const router = useRouter();
+  const currentPath = router.pathname;
+
   return (
     <div className="flex flex-col h-screen w-full">
       {children}
@@ -14,25 +22,36 @@ const PrimaryLayout: React.FC<{ children: React.ReactNode }> = ({
         <Link href={'/solves'}>
           <a
             title="Solves"
-            className="text-white dark:text-black dark:bg-white bg-black font-semibold w-24 flex items-center justify-center rounded-full py-2"
+            className="relative text-white dark:text-black dark:bg-white bg-black font-semibold w-28 gap-2 flex items-center justify-center rounded-full py-2"
           >
-            Solves
+            <ListBulletIcon className="black h-4 w-4" />
+            <span>Solves</span>
+            {currentPath === '/solves' && (
+              <div className="absolute rounded-full bg-black dark:bg-white -top-2 w-1/2 h-1" />
+            )}
           </a>
         </Link>
         <Link href={'/'}>
           <a
             title="Timer"
-            className="font-semibold flex items-center justify-center"
+            className="relative font-semibold flex items-center justify-center"
           >
             <ClockIcon className="text-black dark:text-white h-9 w-9" />
+            {currentPath === '/' && (
+              <div className="absolute rounded-full bg-black dark:bg-white -top-2 w-1/2 h-1" />
+            )}
           </a>
         </Link>
-        <Link href={'/algorithms'}>
+        <Link href={'/study'}>
           <a
             title="Study"
-            className="pointer-events-none text-white dark:text-black dark:bg-white bg-black font-semibold w-24 flex items-center justify-center rounded-full py-2"
+            className="relative text-white dark:text-black dark:bg-white bg-black font-semibold w-28 flex items-center justify-center gap-2 rounded-full py-2"
           >
-            Study (TBD)
+            <BookOpenIcon className="text-white dark:text-black h-4 w-4" />
+            <span>Study</span>
+            {currentPath === '/study' && (
+              <div className="absolute rounded-full bg-black dark:bg-white -top-2 w-1/2 h-1" />
+            )}
           </a>
         </Link>
       </div>
