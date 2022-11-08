c337195e22d3e5ed4e96eef10193ab650ba6c005
Marlou Lopez <dev.marlou.lopez@gmail.com>
2022-11-09 01:37:53 +0800

    modify stat grap height

--- components/Stats/StatsGraph/StatsGraph.tsx
+++ components/Stats/StatsGraph/StatsGraph.tsx
@@ -39,7 +39,7 @@ const StatsGraph: React.FC<StatsGraphProps> = ({ solves }) => {
       <h1 className="font-bold">Time Trend</h1>
       {solves.length > 1 ? (
         <ResponsiveContainer width={'100%'} height={150}>
-          <LineChart width={300} height={150} data={solveTimeStamps}>
+          <LineChart width={300} data={solveTimeStamps}>
             <Line type={'monotone'} dataKey="single" dot={false} strokeWidth={3} />
             <Line type={'monotone'} dataKey="ao5" stroke="#82ca9d" dot={false} strokeWidth={2} />
             <Line type={'monotone'} dataKey="ao12" stroke="#8884d8" dot={false} strokeWidth={2} />
