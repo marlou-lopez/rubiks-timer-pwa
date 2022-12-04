3d3e5abccbe7159ae68437246b12456fba1e0225
Marlou Lopez <dev.marlou.lopez@gmail.com>
2022-12-05 02:10:34 +0800

    fix layout issue for hands

--- components/Timer/TimerHands.tsx
+++ components/Timer/TimerHands.tsx
@@ -4,7 +4,9 @@ const TimerHands: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="flex w-full items-center justify-evenly">
       <Image src={'/images/hand-left.png'} height={150} width={150} alt="Hand left" />
-      <div className="flex w-24 flex-col items-center justify-center">{children}</div>
+      <div className="flex flex-col items-center justify-center mobile-ls:w-[250px]">
+        {children}
+      </div>
       <Image src={'/images/hand-right.png'} height={150} width={150} alt="Hand right" />
     </div>
   );
